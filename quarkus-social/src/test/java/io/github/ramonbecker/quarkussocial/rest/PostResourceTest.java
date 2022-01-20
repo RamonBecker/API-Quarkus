53397181a15b32d19af942d75bb7874a51ae8175
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-20 15:56:45 -0300

    Criando cenário com beforeEach

---
+++ quarkus-social/src/test/java/io/github/ramonbecker/quarkussocial/rest/PostResourceTest.java
@@ -0,0 +1,52 @@
+package io.github.ramonbecker.quarkussocial.rest;
+
+import io.github.ramonbecker.quarkussocial.domain.model.User;
+import io.github.ramonbecker.quarkussocial.domain.repositories.UserRespository;
+import io.github.ramonbecker.quarkussocial.rest.dto.CreatePostRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestHTTPEndpoint(PostResource.class)
+class PostResourceTest {
+
+    @Inject
+    UserRespository userRespository;
+
+    Long userId;
+
+    @BeforeEach
+    @Transactional
+    public void setUP(){
+        var user = new User();
+        user.setName("Fulano");
+        user.setAge(30);
+        userRespository.persist(user);
+        userId = user.getId();
+    }
+
+    @Test
+    @DisplayName("Should create a post for a user")
+    public void createPostTest(){
+        var postRequest = new CreatePostRequest();
+        postRequest.setText("Some text");
+
+        given().
+                contentType(ContentType.JSON)
+                .body(postRequest)
+                .pathParam("userId", userId)
+                .when().post()
+                .then().statusCode(201);
+    }
+
+
+}
