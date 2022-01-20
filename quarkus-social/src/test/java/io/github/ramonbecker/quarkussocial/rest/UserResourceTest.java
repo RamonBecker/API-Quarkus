7f9a5e09d4e9954e1ce74dea411d0c4f93bf355a
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-20 15:11:08 -0300

    Testando o caso de erro de validação

--- quarkus-social/src/test/java/io/github/ramonbecker/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/ramonbecker/quarkussocial/rest/UserResourceTest.java
@@ -1,10 +1,14 @@
 package io.github.ramonbecker.quarkussocial.rest;
 
 import io.github.ramonbecker.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.ramonbecker.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class UserResourceTest {
 
     @Test
-    @DisplayName("should create an user successfully")
+    @DisplayName("Should create an user successfully")
     public void createUserTest() {
         var user = new CreateUserRequest();
         user.setName("Fulano");
@@ -35,4 +39,29 @@ class UserResourceTest {
 
     }
 
+    @Test
+    @DisplayName("Should return error when json is not valid")
+    public void createUserValidationErrorTest(){
+        var user = new CreateUserRequest();
+        user.setName(null);
+        user.setAge(null);
+
+        var response =
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(user)
+                        .when().post("/users")
+                        .then().extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.getStatusCode());
+        assertEquals("Validation error",response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
+
+    //    assertEquals("Age is required",errors.get(0).get("message"));
+      //  assertEquals("Name is required", errors.get(1).get("message"));
+
+    }
 }
